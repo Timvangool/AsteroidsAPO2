28c458d36af36157eeaf35d054d34b35ccb05b6e
Jeebes <progagguitar@hotmail.com>
2015-11-20 10:52:02 +0100

    20-11-2015 controls solution V0.1
    Not finished.. put here for review and fixing

---
+++ ControllsSolution/WiimoteTest2015/WiimoteTest2015/WiimoteTest2015/Game1.cs
@@ -0,0 +1,109 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using WiimoteLib;
+
+namespace WiimoteTest2015
+{
+    /// <summary>
+    /// This is the main type for your game
+    /// </summary>
+    public class Game1 : Microsoft.Xna.Framework.Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        WiimoteHandler wm;
+        Vector2 pos = new Vector2(50, 50);
+        Texture2D sShip;
+
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            base.Initialize();
+            wm = new WiimoteHandler();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            
+            // TODO: use this.Content to load your game content here
+            sShip = this.Content.Load<Texture2D>("spaceship");
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// all content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            // Allows the game to exit
+            if (wm.GetButtonsPressed().Contains("Home"))
+                this.Exit();
+
+            // TODO: Add your update logic here
+            if (wm.GetButtonsPressed().Contains("Up"))
+                pos.X--;
+            if (wm.GetButtonsPressed().Contains("Down"))
+                pos.X++;
+            if (wm.GetButtonsPressed().Contains("Right"))
+                pos.Y--;
+            if (wm.GetButtonsPressed().Contains("Left"))
+                pos.Y++;
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);                       
+
+            // TODO: Add your drawing code here
+            spriteBatch.Begin();
+            spriteBatch.Draw(sShip, pos, Color.White);
+            spriteBatch.End();
+
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ ControllsSolution/WiimoteTest2015/WiimoteTest2015/WiimoteTest2015/WiimoteHandler.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using WiimoteLib;
+using System.Xml;
+
+namespace WiimoteTest2015
+{
+    public class WiimoteHandler
+    {
+        Wiimote wm;
+
+        public WiimoteHandler()
+        {
+            ConnectWiimote();
+
+        }
+
+        private void ConnectWiimote()
+        {
+            wm = new Wiimote();
+            try
+            {
+                wm.Connect();
+            }
+            catch(WiimoteNotFoundException ex)
+            {
+                MessageBox.Show("Wiimote not found: ", ex.Message);
+            }
+            catch(WiimoteException ex)
+            {
+                MessageBox.Show("Wiimote error: ", ex.Message);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Unknown error: ", ex.Message);
+            }
+        }
+
+        public List<string> GetButtonsPressed()
+        {
+            List<string> btnsPressed = new List<string>();
+                if (wm.WiimoteState.ButtonState.A)
+                    btnsPressed.Add("A");
+
+            return btnsPressed;
+        }
+
+
+    }
+}
+
+
+                //if (wm.WiimoteState.ButtonState.A)
+                //    btnsPressed.Add("A");
+                //if (wm.WiimoteState.ButtonState.B)
+                //    btnsPressed.Add("B");
+                //if (wm.WiimoteState.ButtonState.One)
+                //    btnsPressed.Add("1");
+                //if (wm.WiimoteState.ButtonState.Two)
+                //    btnsPressed.Add("2");
+                //if (wm.WiimoteState.ButtonState.Plus)
+                //    btnsPressed.Add("+");
+                //if (wm.WiimoteState.ButtonState.Minus)
+                //    btnsPressed.Add("-");
+                //if (wm.WiimoteState.ButtonState.Up)
+                //    btnsPressed.Add("Up");
+                //if (wm.WiimoteState.ButtonState.Down)
+                //    btnsPressed.Add("Down");
+                //if (wm.WiimoteState.ButtonState.Left)
+                //    btnsPressed.Add("Left");
+                //if (wm.WiimoteState.ButtonState.Right)
+                //    btnsPressed.Add("Right");
+                //if (wm.WiimoteState.ButtonState.Home)
+                //    btnsPressed.Add("Home");
+
