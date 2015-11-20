7b8076b4b745fec774e8ba38b0db005bb93e5d23
Timvangool <timvangool@hotmail.com>
2015-11-20 15:35:02 +0100

    20-11-2015
    PhysicsFrameworkXNA

---
+++ PhysicsFrameworkXNA/PhysicsFrameworkXNA/PhysicsFrameworkXNA/Circle.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace PhysicsFrameworkXNA
+{
+    class Circle
+    {
+        Texture2D pixel; //our pixel texture we will be using to draw primitives
+        GraphicsDevice gd; //graphics device to use
+        SpriteBatch sb; //sprite batch to use
+
+        private Color color;
+        public Vector2 position;
+        public Vector2 velocity;
+        public int radius;
+
+        public Color Color { get; set; }
+
+        public Circle(GraphicsDevice graphicsDevice, SpriteBatch spriteBatch, Vector2 position, Color color, int radius)
+        {
+            this.gd = graphicsDevice;
+            this.sb = spriteBatch;
+            this.position = position;
+            velocity = new Vector2(0, 0);
+            this.color = color;
+            this.radius = radius;
+            pixel = new Texture2D(gd, 1, 1);
+            pixel.SetData(new Color[] {color});
+        }
+
+        public void Draw()
+        {
+            for (int x = (int)position.X - radius; x <= (int)position.X + radius; x++)
+            {
+                for (int y = (int)position.Y - radius; y <= (int)position.Y + radius; y++)
+                {
+                    float dx = x - position.X;
+                    float dy = y - position.Y;
+                    if (dx * dx + dy * dy < radius * radius )
+                    {
+                        sb.Draw(pixel, new Vector2(x, y), color);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ PhysicsFrameworkXNA/PhysicsFrameworkXNA/PhysicsFrameworkXNA/Pixel.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace PhysicsFrameworkXNA
+{
+    class Pixel
+    {
+        Texture2D pixel; //our pixel texture we will be using to draw primitives
+        GraphicsDevice gd; //graphics device to use
+        SpriteBatch sb; //sprite batch to use
+
+        protected Color color;
+        public Vector2 position;
+        public Vector2 velocity;
+
+        public Color Color { get; set; }
+
+        public Pixel(GraphicsDevice graphicsDevice, SpriteBatch spriteBatch, int xposit, int yposit, Color col)
+        {
+            this.gd = graphicsDevice;
+            this.sb = spriteBatch;
+            position = new Vector2(xposit, yposit);
+            velocity = new Vector2(0, 0);
+            color = col;
+            pixel = new Texture2D(gd, 1, 1);
+            pixel.SetData(new Color[] {color});
+        }
+
+        public void Draw()
+        {
+            sb.Draw(pixel, position, color);
+        }
+
+        public void CheckBoundries(int scrnWidth, int scrnHeight)
+        {
+            if (position.Y < 0)
+            {
+                position.Y = 0;
+                velocity.Y *= -1.0f;
+            }
+
+            if (position.Y > scrnHeight)
+            {
+                position.Y = scrnHeight;
+                velocity.Y *= -1.0f;
+            }
+
+            if (position.X < 0)
+            {
+                position.X = 0;
+                velocity.X *= -1.0f;
+            }
+            if (position.X > scrnWidth)
+            {
+                position.X = scrnWidth;
+                velocity.X *= -1.0f;
+            }
+        }
+    }
+}
