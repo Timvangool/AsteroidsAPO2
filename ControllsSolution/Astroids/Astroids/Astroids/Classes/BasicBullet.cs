cb2995f692877c8a33b1ec6e6cfdce759022c78f
Jeebess <vegamascotte@gmail.com>
2015-11-26 16:01:06 +0100

    26-11-2015 Added Thom's solution with added controls V1.0

---
+++ ControllsSolution/Astroids/Astroids/Astroids/Classes/BasicBullet.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace Astroids.Classes
+{
+    class BasicBullet : Weapon
+    {
+        public BasicBullet()
+        {
+            speed = 10;
+            isVisable = true;
+            fadeTime = 75;
+        }
+
+        public override void Load(ContentManager content, Vector2 direction)
+        {
+            pos = pos + direction;
+        }
+
+        public override void Update(GameTime gameTime, Vector2 direction)
+        {
+            pos = pos + (direction * speed);
+            fadeTime--;
+            hitBox = new Rectangle((int)(pos.X - (texture.Width / 2)), (int)(pos.Y - (texture.Height / 2)), texture.Width, texture.Height);
+        }
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(texture, pos, Color.White);
+        }
+    }
+}
