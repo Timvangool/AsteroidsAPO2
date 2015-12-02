459f46177f7b08498ccb9a4d637b739fb41ae92f
thijmenboot <d206846@edu.rocwb.nl>
2015-12-02 12:42:39 +0100

    Dynamic Background

---
+++ Solution/Astroids/Astroids/Astroids/Classes/Background.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Astroids.Classes
+{
+    class Background
+    {
+        private const int starAmount = 100;
+        private Star[] starArray = new Star[starAmount];
+        private GraphicsDevice graphicsManager;
+        private ContentManager content;
+        public Background(GraphicsDevice graphicsDevice, ContentManager content)
+        {
+            this.graphicsManager = graphicsDevice;
+            this.content = content;
+            LoadStars();
+        }
+
+        public void LoadStars()
+        {
+            Random rnd = new Random();
+            for (int i = 0; i < starAmount; i++)
+            {
+                starArray[i] = new Star(graphicsManager, content, rnd);
+            }
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            Random rnd = new Random();
+
+            foreach (Star star in starArray)
+            {
+                int random = rnd.Next(0, 1000);
+
+                if (random == 1)
+                {
+                    star.Draw(spriteBatch, Color.Cyan);
+                }
+                else if(random == 2)
+                {
+                    star.Draw(spriteBatch, Color.LightGray);
+                }
+                else if(random <= 3)
+                {
+                    star.Draw(spriteBatch, Color.White);
+                }
+                else
+                {
+                    star.Draw(spriteBatch, Color.Gray);
+                }
+
+            }
+        }
+    }
+}
--- Solution/Astroids/Astroids/Astroids/Game1.cs
+++ Solution/Astroids/Astroids/Astroids/Game1.cs
@@ -20,13 +20,26 @@ namespace Astroids
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
         Player p;
+        Random r;
+        HUD hud;
+        List<Asteroid> asteroidList, asteroid, newAsteroidList;
+        List<Weapon> killListWep;
+        Vector2 dir;
+        Background background;
+        int numOfAsteroids;
+        
 
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
-            graphics.IsFullScreen = true;
+            //graphics.IsFullScreen = true;
+            graphics.PreferredBackBufferHeight = 500;
+            graphics.PreferredBackBufferWidth = 900;
             Content.RootDirectory = "Content";
+            r = new Random();
             p = new Player();
+            hud = new HUD();
+            numOfAsteroids = 5;
         }
 
         /// <summary>
@@ -38,6 +51,15 @@ namespace Astroids
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
+            asteroidList = new List<Asteroid>();
+            asteroid = new List<Asteroid>();
+            newAsteroidList = new List<Asteroid>();
+            killListWep = new List<Weapon>();
+            for (int i = 0; i < numOfAsteroids; i++)
+            {
+                double angle = r.NextDouble() * 2 * Math.PI;
+                asteroid.Add(new Asteroid(r.Next(1, graphics.PreferredBackBufferWidth), r.Next(1, graphics.PreferredBackBufferHeight), r.Next(1, 4), 3.0f, dir = new Vector2((float)Math.Cos(angle), (float)Math.Sin(angle))));
+            }
 
             base.Initialize();
         }
@@ -51,8 +73,14 @@ namespace Astroids
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
 
-            // TODO: use this.Content to load your game content here
             p.Load(Content);
+            hud.Load(Content);
+            background = new Background(GraphicsDevice, Content);
+
+            foreach (Weapon wep in p.weapList)
+            {
+                wep.Load(Content, p.GetDirection());
+            }
         }
 
         /// <summary>
@@ -72,14 +100,127 @@ namespace Astroids
         protected override void Update(GameTime gameTime)
         {
             // Allows the game to exit
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
+            if (Keyboard.GetState().IsKeyDown(Keys.Escape))
                 this.Exit();
 
-            // TODO: Add your update logic here
             p.Update(gameTime);
+            p.CheckBoundries(GraphicsDevice.Viewport.Width, GraphicsDevice.Viewport.Height);
+            hud.Update(gameTime);
+
+            foreach (Asteroid ast in asteroid)
+            {
+                ast.Update(gameTime);
+                ast.CheckBoundries(graphics.PreferredBackBufferWidth, graphics.PreferredBackBufferHeight);
+
+                if (p.GetPlayerHitbox().Intersects(ast.GetAsteroidHitbox()))
+                {
+                    switch (ast.GetSize())
+                    {
+                        case 1:
+                            asteroidList.Add(ast);
+                            p.SetLives((p.GetLife() - 1));
+                            break;
+                        case 2:
+                            for (int i = 0; i < 2; i++)
+                            {
+                                double angle = r.NextDouble() * 2 * Math.PI;
+                                newAsteroidList.Add(new Asteroid(ast.getXPos(), ast.getYPos(), 1, 3.0f, dir = new Vector2((float)Math.Cos(angle), (float)Math.Sin(angle))));
+                            }
+                            asteroidList.Add(ast);
+                            p.SetLives((p.GetLife() - 1));
+                            break;
+                        case 3:
+                            for (int i = 0; i < 2; i++)
+                            {
+                                double angle = r.NextDouble() * 2 * Math.PI;
+                                newAsteroidList.Add(new Asteroid(ast.getXPos(), ast.getYPos(), 2, 3.0f, dir = new Vector2((float)Math.Cos(angle), (float)Math.Sin(angle))));
+                            }
+                            asteroidList.Add(ast);
+                            p.SetLives((p.GetLife() - 1));
+                            break;
+                        default:
+
+                            break;
+                    }
+                }
+            }
+
+            if (asteroid.Count == 0)
+            {
+                LevelsIncrease();
+            }
+
+            foreach (Asteroid a in newAsteroidList)
+            {
+                asteroid.Add(a);
+            }
+
+            foreach (Asteroid a in asteroidList)
+            {
+                asteroid.Remove(a);
+            }
+
+            asteroidList.Clear();
+            newAsteroidList.Clear();
+
+            foreach (Weapon wep in p.weapList)
+            {
+                wep.Update(gameTime, wep.GetDirection());
+                wep.CheckBoundries(GraphicsDevice.Viewport.Width, GraphicsDevice.Viewport.Height);
+
+                if (wep.GetFadeTime() == 0)
+                {
+                    wep.SetIsVisable(false);
+                    killListWep.Add(wep);
+                }
+
+                foreach (Asteroid a in asteroid)
+                {
+                    if (wep.GetHitbox().Intersects(a.GetAsteroidHitbox()))
+                    {
+                         switch (a.GetSize())
+                         {
+                             case 1:
+                            asteroidList.Add(a);
+                            killListWep.Add(wep);
+                            hud.SetScore(10);
+                            break;
+                        case 2:
+                            for (int i = 0; i < 2; i++)
+                            {
+                                double angle = r.NextDouble() * 2 * Math.PI;
+                                newAsteroidList.Add(new Asteroid(a.getXPos(), a.getYPos(), 1, 3.0f, dir = new Vector2((float)Math.Cos(angle), (float)Math.Sin(angle))));
+                            }
+                            asteroidList.Add(a);
+                            killListWep.Add(wep);
+                            hud.SetScore(25);
+                            break;
+                        case 3:
+                            for (int i = 0; i < 2; i++)
+                            {
+                                double angle = r.NextDouble() * 2 * Math.PI;
+                                newAsteroidList.Add(new Asteroid(a.getXPos(), a.getYPos(), 2, 3.0f, dir = new Vector2((float)Math.Cos(angle), (float)Math.Sin(angle))));
+                            }
+                            asteroidList.Add(a);
+                            killListWep.Add(wep);
+                            hud.SetScore(50);
+                            break;
+                        default:
+
+                            break;
+                         }
+                    }
+                }
+            }
+
+            foreach (Weapon weap in killListWep)
+            {
+                p.weapList.Remove(weap);
+            }
 
             base.Update(gameTime);
         }
+                    
 
         /// <summary>
         /// This is called when the game should draw itself.
@@ -87,14 +228,30 @@ namespace Astroids
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(Color.Black);
+
             spriteBatch.Begin();
+            background.Draw(spriteBatch);
+            foreach (Weapon wep in p.weapList)
+            {
+                wep.Draw(spriteBatch);
+            }
+            foreach (Asteroid a in asteroid)
+            {
+                a.Draw(spriteBatch, Content);
+            }
             p.Draw(spriteBatch);
+            hud.Draw(spriteBatch, p.GetLife());
             spriteBatch.End();
 
-            // TODO: Add your drawing code here
-
             base.Draw(gameTime);
+        }
+
+        public void LevelsIncrease()
+        {
+            numOfAsteroids += r.Next(0, 3);
+            hud.SetLevel(1);
+            Initialize();
         }
     }
 }
