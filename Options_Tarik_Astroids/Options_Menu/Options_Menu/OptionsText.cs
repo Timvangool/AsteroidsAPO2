a91c1b0e83aa46303f2ca7f4a74392225590ef5b
TarikHaci <tarikhaci@outlook.com>
2015-12-08 12:35:31 +0100

    Options_AstroidsV2 8-12-2015
    Meld buggs ;)

---
+++ Options_Tarik_Astroids/Options_Menu/Options_Menu/OptionsText.cs
@@ -0,0 +1,156 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Options_Menu
+{
+    class OptionsText
+    {
+        GraphicsDeviceManager graphics;
+        Texture2D txBackground;
+        Texture2D txSelectArrow;
+        Texture2D txBack;
+        SpriteFont spriteFont;
+
+        string textHeader;
+        string textSound;
+        string textResolution;
+        string textAlias;
+        string textAliasOn;
+        string textAliasOff;
+
+        Vector2 posHeader;
+        Vector2 posSelectArrow;
+        Vector2 posBack;
+        Vector2 posSound;
+        Vector2 posResolution;
+        Vector2 posAlias;
+        Vector2 posAliasOn;
+        Vector2 posAliasOff;
+        Vector2 posHeaderConverted;
+        Vector2 posSoundConverted;
+        Vector2 posResolutionConverted;
+        Vector2 posAliasConverted;
+        Vector2 posAliasOnConverted;
+        Vector2 posAliasOffConverted;
+        Vector2 sizeBack;
+        Vector2 sizeSelectArrow;
+
+        Color col;
+
+        Rectangle recBack;
+        Rectangle recSelectArrow;
+
+        bool mouseReleased = true;
+
+        float newPos;
+
+        public OptionsText(GraphicsDeviceManager graphics, Texture2D txBackground, Texture2D txBack, Vector2 posBack, Vector2 sizeBack, Texture2D txSelectArrow, Vector2 posSelectArrow, Vector2 sizeSelectArrow, SpriteFont spriteFont, string textHeader, Vector2 posHeader, string textSound, Vector2 posSound, string textResolution, Vector2 posResolution, string textAlias, Vector2 posAlias, string textAliasOn, Vector2 posAliasOn, string textAliasOff, Vector2 posAliasOff, Color col)
+        {
+            this.graphics = graphics;
+            this.txBackground = txBackground;
+            this.txBack = txBack;
+            this.txSelectArrow = txSelectArrow;
+            this.sizeSelectArrow = sizeSelectArrow;
+            this.posBack = posBack;
+            this.sizeBack = sizeBack;
+            this.spriteFont = spriteFont;
+
+            this.textHeader = textHeader;
+            this.textSound = textSound;
+            this.textResolution = textResolution;
+            this.textAlias = textAlias;
+            this.textAliasOn = textAliasOn;
+            this.textAliasOff = textAliasOff;
+
+            this.posSound = posSound;
+            this.posHeader = posHeader;
+            this.posSelectArrow = posSelectArrow;
+            this.posResolution = posResolution;
+            this.posAlias = posAlias;
+            this.posAliasOn = posAliasOn;
+            this.posAliasOff = posAliasOff;
+
+            this.col = col;
+            newPos = posSelectArrow.Y;
+            Init();
+        }
+
+        public void Init()
+        {
+            recBack = new Rectangle(Convert.ToInt32(graphics.PreferredBackBufferWidth / posBack.X), Convert.ToInt32(graphics.PreferredBackBufferHeight / posBack.Y), (int)sizeBack.X, (int)sizeBack.Y);
+            recSelectArrow = new Rectangle(Convert.ToInt32(graphics.PreferredBackBufferWidth / posSelectArrow.X), Convert.ToInt32(graphics.PreferredBackBufferHeight / newPos), (int)sizeSelectArrow.X, (int)sizeSelectArrow.Y);
+            posSoundConverted = new Vector2(graphics.PreferredBackBufferWidth / posSound.X, graphics.PreferredBackBufferHeight / posSound.Y);
+            posHeaderConverted = new Vector2(graphics.PreferredBackBufferWidth / posHeader.X, graphics.PreferredBackBufferHeight / posHeader.Y);
+            posResolutionConverted = new Vector2(graphics.PreferredBackBufferWidth / posResolution.X, graphics.PreferredBackBufferHeight / posResolution.Y);
+            posAliasConverted = new Vector2(graphics.PreferredBackBufferWidth / posAlias.X, graphics.PreferredBackBufferHeight / posAlias.Y);
+            posAliasOnConverted = new Vector2(graphics.PreferredBackBufferWidth / posAliasOn.X, graphics.PreferredBackBufferHeight / posAliasOn.Y);
+            posAliasOffConverted = new Vector2(graphics.PreferredBackBufferWidth / posAliasOff.X, graphics.PreferredBackBufferHeight / posAliasOff.Y);
+        }
+
+        public void UpdateSelect(int number)
+        {
+            
+            switch (number)
+            {
+                case 0:
+                    {
+                        newPos = posSelectArrow.Y;
+                        break;
+                    }
+                case 1:
+                    {
+                        newPos = posSelectArrow.Y - 0.68f;
+                        break;
+                    }
+                case 2:
+                    {
+                        newPos = posSelectArrow.Y - 1.15f;
+                        break;
+                    }
+                case 3:
+                    {
+                        newPos = posSelectArrow.Y - 1.4f;
+                        break;
+                    }
+            }
+            recSelectArrow = new Rectangle(Convert.ToInt32(graphics.PreferredBackBufferWidth / posSelectArrow.X), Convert.ToInt32(graphics.PreferredBackBufferHeight / newPos), (int)sizeSelectArrow.X, (int)sizeSelectArrow.Y);
+        }
+
+        public bool Update(MouseState mouse)
+        {
+            Rectangle mouseRec = new Rectangle((int)mouse.X, (int)mouse.Y, (int)sizeBack.X, (int)sizeBack.Y);
+            if (recBack.Intersects(mouseRec))
+            {
+                if (mouse.LeftButton == ButtonState.Pressed && mouseReleased == true)
+                {
+                    mouseReleased = false;
+                    return true;
+                }
+
+            }
+            if (mouse.LeftButton == ButtonState.Released)
+            {
+                mouseReleased = true;
+            }
+            return false;
+        }
+
+        public void Draw(SpriteBatch sprite)
+        {
+            sprite.Draw(txBackground, new Rectangle(0, 0, graphics.PreferredBackBufferWidth, graphics.PreferredBackBufferHeight), Color.White);
+            sprite.Draw(txBack, recBack, Color.White);
+            sprite.Draw(txSelectArrow, recSelectArrow, Color.White);
+            sprite.DrawString(spriteFont, textHeader, posHeaderConverted, col);
+            sprite.DrawString(spriteFont, textResolution, posResolutionConverted, col);
+            sprite.DrawString(spriteFont, textSound, posSoundConverted, col);
+            sprite.DrawString(spriteFont, textAlias, posAliasConverted, col);
+            sprite.DrawString(spriteFont, textAliasOn, posAliasOnConverted, col);
+            sprite.DrawString(spriteFont, textAliasOff, posAliasOffConverted, col);
+        }
+    }
+}
---
+++ Options_Tarik_Astroids/Options_Menu/Options_Menu/TResolutionOption.cs
@@ -0,0 +1,151 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Options_Menu
+{
+    class TResolutionOption
+    {
+        GraphicsDeviceManager graphics;
+
+        string[][] arResolutions = new string[][]
+        {
+            new string[] {"  1920 x 1080", "1920", "1080"},
+            new string[] {"  1680 x 1050", "1680", "1050"},
+            new string[] {"  1600 x 900", "1600", "900"},
+            new string[] {"  1440 x 900", "1440", "900"},
+            new string[] {"  1366 x 768", "1366", "786"},
+            new string[] {"  1280 x 1024", "1280", "1024"},
+            new string[] {"  1280 x 800", "1280", "800"},
+            new string[] {"  1024 x 786", "1024", "786"}
+        };
+        int arrayNumber = 6;
+        Texture2D txResolutionBar;
+        Texture2D txArrowLeft;
+        Texture2D txArrowRight;
+        Vector2 posResolutionBar;
+        Vector2 sizeResolutionBar;
+        Vector2 posArrowLeft;
+        Vector2 posArrowRight; 
+        Vector2 sizeArrow;
+        Vector2 temp;
+
+        Rectangle recArrowLeft;
+        Rectangle recArrowRight;
+        Rectangle recResolutionBar;
+        Color col;
+        bool mouseReleased = true;
+        bool resolutionChanged = false;
+
+        public TResolutionOption(GraphicsDeviceManager graphics, Texture2D txResolutionBar, Texture2D txArrowLeft, Texture2D txArrowRight, Vector2 posResolutionBar, Vector2 sizeResolutionBar, Vector2 posArrowLeft, Vector2 posArrowRight, Vector2 sizeArrow, Color col)
+        {
+            this.graphics = graphics;
+            this.txResolutionBar = txResolutionBar;
+            this.txArrowLeft = txArrowLeft;
+            this.txArrowRight = txArrowRight;
+            this.posResolutionBar = posResolutionBar;
+            this.sizeResolutionBar = sizeResolutionBar;
+            this.posArrowLeft = posArrowLeft;
+            this.posArrowRight = posArrowRight;
+            this.sizeArrow = sizeArrow;
+            this.sizeResolutionBar = sizeResolutionBar;
+            this.col = col;
+            this.temp = posResolutionBar;
+            Init();
+
+        }
+
+        public void Init()
+        {
+            posResolutionBar = new Vector2(graphics.PreferredBackBufferWidth / temp.X, graphics.PreferredBackBufferHeight / temp.Y);
+            recArrowLeft = new Rectangle(Convert.ToInt32(graphics.PreferredBackBufferWidth / posArrowLeft.X), Convert.ToInt32(graphics.PreferredBackBufferHeight / posArrowLeft.Y), (int)sizeArrow.X, (int)sizeArrow.Y);
+            recArrowRight = new Rectangle(Convert.ToInt32(graphics.PreferredBackBufferWidth / posArrowRight.X), Convert.ToInt32(graphics.PreferredBackBufferHeight / posArrowRight.Y), (int)sizeArrow.X, (int)sizeArrow.Y);
+            recResolutionBar = new Rectangle(Convert.ToInt32(posResolutionBar.X), Convert.ToInt32(posResolutionBar.Y), (int)sizeResolutionBar.X, (int)sizeResolutionBar.Y);
+            
+        }
+
+        public void SelectLeft()
+        {
+            if (arrayNumber != arResolutions.Count() - 1)
+            {
+                arrayNumber++;
+                ChangeResolution(new Vector2(Convert.ToInt32(arResolutions[arrayNumber][1]), Convert.ToInt32(arResolutions[arrayNumber][2])));
+            }
+        }
+
+        public void SelectRight()
+        {
+            if (arrayNumber != 0)
+            {
+                arrayNumber--;
+                ChangeResolution(new Vector2(Convert.ToInt32(arResolutions[arrayNumber][1]), Convert.ToInt32(arResolutions[arrayNumber][2])));
+            }
+        }
+
+        public bool GetResolutionChanged()
+        {
+            return resolutionChanged;
+        }
+
+        public void ChangeResolution(Vector2 screen)
+        {
+            if(resolutionChanged == false)
+            {
+                resolutionChanged = true;
+            }
+            graphics.PreferredBackBufferWidth = (int)screen.X;
+            graphics.PreferredBackBufferHeight = (int)screen.Y;
+            graphics.ApplyChanges();
+        }
+
+        public void Update(MouseState mouse)
+        {
+            Rectangle mouseRec = new Rectangle((int)mouse.X, (int)mouse.Y, (int)sizeArrow.X, (int)sizeArrow.Y);
+            if (recArrowLeft.Intersects(mouseRec))
+            {
+                if (mouse.LeftButton == ButtonState.Pressed && mouseReleased == true)
+                {
+                    if (arrayNumber != arResolutions.Count() - 1)
+                    {
+                        arrayNumber++;
+                        ChangeResolution(new Vector2(Convert.ToInt32(arResolutions[arrayNumber][1]), Convert.ToInt32(arResolutions[arrayNumber][2])));
+                    }
+
+                    mouseReleased = false;
+                }
+
+            }
+            if (recArrowRight.Intersects(mouseRec))
+            {
+                if (mouse.LeftButton == ButtonState.Pressed && mouseReleased == true)
+                {
+                    if (arrayNumber != 0)
+                    {
+                        arrayNumber--;
+                        ChangeResolution(new Vector2(Convert.ToInt32(arResolutions[arrayNumber][1]), Convert.ToInt32(arResolutions[arrayNumber][2])));
+                    }
+
+                    mouseReleased = false;
+                }
+
+            }
+            if (mouse.LeftButton == ButtonState.Released)
+            {
+                mouseReleased = true;
+            }
+        }
+
+        public void Draw(SpriteBatch sprite, SpriteFont spriteFont)
+        {
+            sprite.Draw(txArrowLeft, recArrowLeft, col);
+            sprite.Draw(txArrowRight, recArrowRight, col);
+            sprite.Draw(txResolutionBar, recResolutionBar, col);
+            sprite.DrawString(spriteFont, arResolutions[arrayNumber][0], posResolutionBar, Color.Black);
+        }
+
+    }
+}
